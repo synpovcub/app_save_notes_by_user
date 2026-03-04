d5a414582d379fa9a5428735c89fdcaf07036096
synpovcub <synpovcub@gmail.com>
2026-03-03 20:18:01 -0600

    change website tabs if user is authenticated

--- website/auth.py
+++ website/auth.py
@@ -22,7 +22,7 @@ def login():
                 flash('Incorrect password, try again', category = 'error')
         else:
             flash('Email does not exist', category = 'error')
-    return render_template("login.html", boolean = True)
+    return render_template("login.html", user = current_user)
 
 @auth.route('/logout')
 @login_required
@@ -59,4 +59,4 @@ def sign_up():
 
 
             
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user = current_user)
