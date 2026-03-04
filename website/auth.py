6171e39689d63a19eb3e296f63fc24a733aa16b4
synpovcub <synpovcub@gmail.com>
2026-03-03 20:12:37 -0600

    Add remember login

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -11,10 +12,11 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        
+        user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category = 'success')
+                login_user(user, remember = True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again', category = 'error')
@@ -23,8 +25,10 @@ def login():
     return render_template("login.html", boolean = True)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 @auth.route('/sign-up', methods = ['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
@@ -49,6 +53,7 @@ def sign_up():
             new_user = User(email = email, first_name = first_name, password = generate_password_hash(password1,method = 'pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember = True)
             flash('nice', category = 'success')
             return redirect(url_for('views.home'))
 
