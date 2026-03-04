eeadf237a4b1fe267f1547d4a44be52643c7cace
synpovcub <synpovcub@gmail.com>
2026-03-03 19:19:50 -0600

    Adding DB creation

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,24 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category = 'success')
+            else:
+                flash('Incorrect password, try again', category = 'error')
+        else:
+            flash('Email does not exist', category = 'error')
     return render_template("login.html", boolean = True)
 
 @auth.route('/logout')
@@ -15,19 +28,29 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email = email).first()
+
+        if user:
+            flash('Email already exists', category = 'error')
+        elif len(email) < 4:
             flash('Email short', category = 'error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('too short', category = 'error')
         elif password1 != password2:
             flash('dont match', category = 'error')
         elif len(password1) <7:
             flash('short pass', category = 'error')
         else:
+            new_user = User(email = email, first_name = first_name, password = generate_password_hash(password1,method = 'pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('nice', category = 'success')
+            return redirect(url_for('views.home'))
+
+
             
     return render_template("sign_up.html")
